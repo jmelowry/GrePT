4d06c11885c74ec9b4e29f4fe80da984570cae92
= <08.hockey_stalls@icloud.com>
2024-04-03 22:18:07 -0700

    coded some codes

---
+++ gptgrep.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# cli tool where you describe a pattern and it will search for it in the GPT-3 model
+# usage: gptgrep [the pattern you want to grep]
+# example: 
+# $ gptgrep "include anything that starts with a capital J and ends with a lowercase e"
+# $ grep '^J.*e$'
+# $ echo -e "Jme\nand Jam" | grep '^J.*e$'
+# Jme
+
+import sys
+import re
+import argparse
+
+def query_gpt(described_pattern):
+    # query GPT-3.5 Turbo with the described pattern
+    pass
+
+def validate_pattern(recommended_pattern):
+    # validate the recommended pattern
+    pass
+
+def main():
+    parser = argparse.ArgumentParser(description='Grep for patterns in GPT-3.5 Turbo')
+    parser.add_argument('-x', '--explain', action='store_true', help='Break down what the pattern is doing')
+    parser.add_argument('-y', '--yolo', action='store_true', help='grep first, ask questions later')
+    args = parser.parse_args()
+    
+    described_pattern = ' '.join(sys.argv[1:])
